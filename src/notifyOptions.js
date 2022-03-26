1222eb6c6eee6fe9e10e41134787b9222991d53a
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-26 18:11:16 +0200

    clean up

--- src/notifyOptions.js
+++ src/notifyOptions.js
@@ -1,9 +1,6 @@
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export const notifyOptions = {
   opacity: 0.9,
   clickToClose: true,
   timeout: 2000,
   showOnlyTheLastOne: true,
 };
-
-// export const notify = Notify;
